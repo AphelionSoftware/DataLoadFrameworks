626e2464d8e878ac23d179dbaeefe9e82fc7e2fb
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-24 20:05:36 +0000

    App settings

--- Excel Update/OpenXML/OpenXML/Form1.Designer.cs
+++ Excel Update/OpenXML/OpenXML/Form1.Designer.cs
@@ -28,82 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtFileLocation = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnInsertTopLine = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtSheet = new System.Windows.Forms.TextBox();
             this.txtText = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.txtSQLConn = new System.Windows.Forms.TextBox();
+            this.txtSheet = new System.Windows.Forms.TextBox();
+            this.txtFileLocation = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // txtFileLocation
-            // 
-            this.txtFileLocation.Location = new System.Drawing.Point(286, 13);
-            this.txtFileLocation.Name = "txtFileLocation";
-            this.txtFileLocation.Size = new System.Drawing.Size(533, 22);
-            this.txtFileLocation.TabIndex = 0;
-            this.txtFileLocation.Text = "C:\\Dropbox (Data)\\Clients\\Meerkat\\CARE Somalia\\Data\\StagingTemplates\\Export\\Stagi" +
-    "ng.Activity.xlsx";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "File Location";
             // 
             // btnInsertTopLine
             // 
-            this.btnInsertTopLine.Location = new System.Drawing.Point(286, 119);
+            this.btnInsertTopLine.Location = new System.Drawing.Point(991, 70);
+            this.btnInsertTopLine.Margin = new System.Windows.Forms.Padding(2);
             this.btnInsertTopLine.Name = "btnInsertTopLine";
-            this.btnInsertTopLine.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertTopLine.Size = new System.Drawing.Size(56, 45);
             this.btnInsertTopLine.TabIndex = 2;
-            this.btnInsertTopLine.Text = "Insert";
+            this.btnInsertTopLine.Text = "Insert Lines";
             this.btnInsertTopLine.UseVisualStyleBackColor = true;
             this.btnInsertTopLine.Click += new System.EventHandler(this.btnInsertTopLine_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Location = new System.Drawing.Point(10, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Sheet Name";
             // 
-            // txtSheet
-            // 
-            this.txtSheet.Location = new System.Drawing.Point(286, 41);
-            this.txtSheet.Name = "txtSheet";
-            this.txtSheet.Size = new System.Drawing.Size(533, 22);
-            this.txtSheet.TabIndex = 4;
-            this.txtSheet.Text = "Sheet1";
-            // 
             // txtText
             // 
-            this.txtText.Location = new System.Drawing.Point(286, 73);
+            this.txtText.Location = new System.Drawing.Point(214, 59);
+            this.txtText.Margin = new System.Windows.Forms.Padding(2);
             this.txtText.Name = "txtText";
-            this.txtText.Size = new System.Drawing.Size(533, 22);
+            this.txtText.Size = new System.Drawing.Size(741, 20);
             this.txtText.TabIndex = 6;
             this.txtText.Text = "For internal use only. Do not fill in or change";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 75);
+            this.label3.Location = new System.Drawing.Point(10, 61);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.Size = new System.Drawing.Size(28, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Text";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "SQL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(972, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Insert Programme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 107);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "SQL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 260);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Excel";
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::OpenXML.Properties.Settings.Default, "txtExcel_Text", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtExcel.Location = new System.Drawing.Point(214, 260);
+            this.txtExcel.Margin = new System.Windows.Forms.Padding(2);
+            this.txtExcel.Multiline = true;
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.Size = new System.Drawing.Size(741, 79);
+            this.txtExcel.TabIndex = 12;
+            this.txtExcel.Text = global::OpenXML.Properties.Settings.Default.txtExcel_Text;
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::OpenXML.Properties.Settings.Default, "txtSQL_Text", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSQL.Location = new System.Drawing.Point(214, 107);
+            this.txtSQL.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSQL.Multiline = true;
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.Size = new System.Drawing.Size(741, 149);
+            this.txtSQL.TabIndex = 10;
+            this.txtSQL.Text = global::OpenXML.Properties.Settings.Default.txtSQL_Text;
+            // 
+            // txtSQLConn
+            // 
+            this.txtSQLConn.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::OpenXML.Properties.Settings.Default, "txtConn_Text", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSQLConn.Location = new System.Drawing.Point(214, 83);
+            this.txtSQLConn.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSQLConn.Name = "txtSQLConn";
+            this.txtSQLConn.Size = new System.Drawing.Size(741, 20);
+            this.txtSQLConn.TabIndex = 7;
+            this.txtSQLConn.Text = global::OpenXML.Properties.Settings.Default.txtConn_Text;
+            // 
+            // txtSheet
+            // 
+            this.txtSheet.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::OpenXML.Properties.Settings.Default, "txtSheet_Text", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSheet.Location = new System.Drawing.Point(214, 33);
+            this.txtSheet.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSheet.Name = "txtSheet";
+            this.txtSheet.Size = new System.Drawing.Size(741, 20);
+            this.txtSheet.TabIndex = 4;
+            this.txtSheet.Text = global::OpenXML.Properties.Settings.Default.txtSheet_Text;
+            // 
+            // txtFileLocation
+            // 
+            this.txtFileLocation.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::OpenXML.Properties.Settings.Default, "txtFileLocation_Text", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtFileLocation.Location = new System.Drawing.Point(214, 11);
+            this.txtFileLocation.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFileLocation.Name = "txtFileLocation";
+            this.txtFileLocation.Size = new System.Drawing.Size(741, 20);
+            this.txtFileLocation.TabIndex = 0;
+            this.txtFileLocation.Text = global::OpenXML.Properties.Settings.Default.txtFileLocation_Text;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 253);
+            this.ClientSize = new System.Drawing.Size(1069, 415);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtExcel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSQL);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSQLConn);
             this.Controls.Add(this.txtText);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtSheet);
@@ -111,8 +206,10 @@
             this.Controls.Add(this.btnInsertTopLine);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtFileLocation);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +224,13 @@
         private System.Windows.Forms.TextBox txtSheet;
         private System.Windows.Forms.TextBox txtText;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSQLConn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSQL;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtExcel;
     }
 }
 
--- Excel Update/OpenXML/OpenXML/Form1.cs
+++ Excel Update/OpenXML/OpenXML/Form1.cs
@@ -21,9 +21,9 @@ namespace OpenXML
     public partial class Form1 : Form
     {
         public string srcDBConn = "Data Source=.\\sql2012;Initial Catalog=Meerkat;Integrated Security=True";
-        SqlConnection srcConn;
+        SqlConnection srcConn = new SqlConnection();
         
-        string sSQL = "SELECT CAST([p].[Programme_ID] AS NVARCHAR(MAX)),CAST([p].[BusinessKey] AS NVARCHAR(MAX)),CAST([p].[Code] AS NVARCHAR(MAX)),CAST([p].[LongName] AS NVARCHAR(MAX)),CAST([p].[ProgrammeSiteName] AS NVARCHAR(MAX)),CAST([p].[ShortName] AS NVARCHAR(MAX)),CAST([p].[TextDescription] AS NVARCHAR(MAX)) FROM  [app].[Programme] AS p ";
+        string strSQL = "SELECT CAST([p].[Programme_ID] AS NVARCHAR(MAX)),CAST([p].[BusinessKey] AS NVARCHAR(MAX)),CAST([p].[Code] AS NVARCHAR(MAX)),CAST([p].[LongName] AS NVARCHAR(MAX)),CAST([p].[ProgrammeSiteName] AS NVARCHAR(MAX)),CAST([p].[ShortName] AS NVARCHAR(MAX)),CAST([p].[TextDescription] AS NVARCHAR(MAX)) FROM  [app].[Programme] AS p ";
         public Form1()
         {
             InitializeComponent();
@@ -74,10 +74,168 @@ namespace OpenXML
                 worksheet.Save();
             }
         }
-            
-    
 
-        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertIntoExcel(this.txtSQLConn.Text, this.txtFileLocation.Text, this.txtSQL.Text, this.txtExcel.Text, this.txtSheet.Text, 3);
+        }
+
+        #region Ugly code from SDK
+        private static int InsertSharedStringItem(string text, SharedStringTablePart shareStringPart)
+        {
+
+            // If the part does not contain a SharedStringTable, create one.
+            if (shareStringPart.SharedStringTable == null)
+            {
+                shareStringPart.SharedStringTable = new SharedStringTable();
+            }
+            int i = 0;
+
+            // Iterate through all the items in the SharedStringTable. If the text already exists, return its index.
+            foreach (SharedStringItem item in shareStringPart.SharedStringTable.Elements<SharedStringItem>())
+            {
+                if (item.InnerText == text)
+                {
+                    return i;
+                }
+                i++;
+            }
+
+            // The text does not exist in the part. Create the SharedStringItem and return its index.
+            shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
+            shareStringPart.SharedStringTable.Save();
+            return i;
+        } 
+      
+#endregion
+
+        private int InsertIntoExcel(string pConn, string pFileLocation, string pSQL, string pExcelQuery, string pSheetName, int pRow)
+        {
+            DateTime dTime = DateTime.Now;
+            if (srcConn.ConnectionString != pConn)
+            {
+                if (srcConn.State == ConnectionState.Open)
+                {
+                    srcConn.Close();
+                }
+            }
             
+            if (srcConn.State != ConnectionState.Open)
+            {
+                srcConn.ConnectionString = pConn;
+                srcConn.Open();
+            }
+            SqlCommand comm = new SqlCommand(pSQL);
+            comm.Connection = srcConn;
+            DataTable dt = new DataTable();
+            dt.Load(comm.ExecuteReader());
+
+            string cl = "";
+            uint iRow = (uint)pRow;
+            int index;
+            Cell cell;
+           
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(pFileLocation, true))
+            {
+                IEnumerable<Sheet> sheets = document.WorkbookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == pSheetName);
+                if (sheets.Count() == 0)
+                {
+                    return -1;
+                }
+
+                WorksheetPart worksheetPart = (WorksheetPart)document.WorkbookPart.GetPartById(sheets.First().Id);
+                Worksheet worksheet = worksheetPart.Worksheet;
+                SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+                string cellReference ;
+
+                foreach (DataRow dr in dt.Rows)
+                {
+
+                    Row row;
+                    if (sheetData.Elements<Row>().Where(r => r.RowIndex == iRow).Count() != 0)
+                    {
+                        row = sheetData.Elements<Row>().Where(r => r.RowIndex == iRow).First();
+                    }
+                    else
+                    {
+                        row = new Row() { RowIndex = iRow };
+                        sheetData.Append(row);
+                    }
+                    for (int idx = 0; idx < dt.Columns.Count; idx++)
+                    {
+                        #region Get data
+                        SharedStringTablePart shareStringPart;
+                        if (document.WorkbookPart.GetPartsOfType<SharedStringTablePart>().Count() > 0)
+                        {
+                            shareStringPart = document.WorkbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                        }
+                        else
+                        {
+                            shareStringPart = document.WorkbookPart.AddNewPart<SharedStringTablePart>();
+                        }
+
+                        if (idx >= 26)
+                        {
+                            cl = "A" + Convert.ToString(Convert.ToChar(65 + idx - 26));
+                        }
+                        else
+                        {
+                            cl = Convert.ToString(Convert.ToChar(65 + idx));
+                        }
+
+                        #endregion
+
+
+                        // Insert the text into the SharedStringTablePart.
+
+                        /*
+                        index = InsertSharedStringItem(Convert.ToString(dr[idx]), shareStringPart);
+                        */
+
+                        index = shareStringPart.SharedStringTable.Count();
+                        shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(Convert.ToString(dr[idx]))));
+                        shareStringPart.SharedStringTable.Save();
+                        
+
+                        cellReference = cl + iRow;
+                        //cell = InsertCellInWorksheet(cl, iRow, worksheetPart);
+
+                        // If there is not a cell with the specified column name, insert one. 
+                        /*if (row.Elements<Cell>().Where(c => c.CellReference.Value == cl + iRow).Count() > 0)
+                        {
+                            cell = row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+                        }
+                        else
+                        {
+                        */
+                            // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
+                            Cell refCell = null;
+
+                            cell = new Cell() { CellReference = cellReference };
+                            row.InsertBefore(cell, refCell);
+                        //}
+                        cell.CellValue = new CellValue(index.ToString());
+                        cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+                        
+                    }
+                    iRow++;
+                    if (iRow % 1000 == 0)
+                    {
+                        worksheet.Save();
+                    }
+                }
+                worksheet.Save();
+            }
+
+            return (DateTime.Now - dTime).Seconds;
+             
+        }
+
+       
     }
 }
